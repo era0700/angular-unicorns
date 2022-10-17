bbe9e5e2e7c8be901fb63d98d35a89087ac17422
ping711 <era@e-soft.al>
2022-10-17 17:58:13 +0200

    created user component

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
+
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',

437e6d9bc25cd8200593eeebfe3ef37e3c346fd7
era0700 <myrtezaera@gmail.com>
2022-10-17 21:47:25 +0200

    firebase auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import {AngularFireModule} from '@angular/fire';
 
 import { AppComponent } from './app.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
@@ -14,6 +15,7 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FirebaseService } from './services/firebase.service';
 
 @NgModule({
   declarations: [
@@ -27,11 +29,20 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
     BrowserModule,
     AppRoutingModule,
     ButtonsModule.forRoot(),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAJBxR7H96PbjFsYmN83rKACprlE8yeSiY",
+      authDomain: "angular-uniqueorns.firebaseapp.com",
+      projectId: "angular-uniqueorns",
+      storageBucket: "angular-uniqueorns.appspot.com",
+      messagingSenderId: "879596367099",
+      appId: "1:879596367099:web:898234d1be29b9d0b7945e"
+    }),
+
     ReactiveFormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore())
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
